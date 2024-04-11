6493a1729d4792a3641ef5fb0336a244ca4c7d91
enversaglam <enversaglam@gmail.com>
2024-04-11 17:18:23 +0200

    21.gün E dosyalari ornek

--- src/day21_MultiDimensionalArrays_ArrayLists/E06_ArrayList.java
+++ src/day21_MultiDimensionalArrays_ArrayLists/E06_ArrayList.java
@@ -1,9 +1,22 @@
 package day21_MultiDimensionalArrays_ArrayLists;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class E06_ArrayList {
     public static void main(String[] args) {
         // Kullanicidan girmek istedigi kadar isim alip
         // girilen isimleri kaydedin
         // Kullanici bitirmek icin q'ya basmalidir
+        List<String> isimler = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        String isim = "";
+
+        while(isim.equalsIgnoreCase("q")){
+            System.out.println("Listeye eklemek icin isim giriniz... \nbitirmek icin q'ya basiniz...");
+            isim = scanner.nextLine();
+        }
     }
 }
